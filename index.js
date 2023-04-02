b997ae10c9a0ab58a505f976cd9b57e98624133b
teregiray <dantonengier@mail.ru>
2023-04-02 17:19:44 +0300

    try deploy firebase

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var button = document.getElementById("send_click");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function dataSend() {
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDyg3LGtEwqleGS06x-6K-9jYX_0oHyUos",
+  authDomain: "test-76fad.firebaseapp.com",
+  projectId: "test-76fad",
+  storageBucket: "test-76fad.appspot.com",
+  messagingSenderId: "193534079381",
+  appId: "1:193534079381:web:479b33b4c04e9a69d7469c",
+  measurementId: "G-VJFJV12CVR"
+};
 
-    var request = new XMLHttpRequest();
-    var ssid = document.getElementById("ssid").value;
-    var password = document.getElementById("password").value;
-   request.open('GET', '/send_click?ssid=' + ssid + '&password=' + password, false);
-   request.send();
-   }
-    const functionButton = button.addEventListener('click', dataSend);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
